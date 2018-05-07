2b93fd244e88cb4325ea500cd12f28f6e4a39865
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2018-05-07 19:29:42 +0900

    add function to post message

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+const db = admin.database();
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/messages', (req, res) => {
+  const message = req.body.message;
+  db.ref('/messages').push(message).then(() => {
+    res.send(message);
+  });
+});
+
+exports.app = functions.https.onRequest(app);
